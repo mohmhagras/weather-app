9b6f1a647aa9a99c2fb241f663d8fa60e04132c2
Mohamed Hagras <mohamed.hagras2002@gmail.com>
2023-03-07 12:16:18 +0200

    completed UV index info box

--- src/app/current-highlights/current-highlights.component.ts
+++ src/app/current-highlights/current-highlights.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SpeedUnit } from '../shared/enums/speed-unit';
 
 @Component({
@@ -6,12 +6,13 @@ import { SpeedUnit } from '../shared/enums/speed-unit';
   templateUrl: './current-highlights.component.html',
   styleUrls: ['./current-highlights.component.scss'],
 })
-export class CurrentHighlightsComponent {
+export class CurrentHighlightsComponent implements OnInit {
   @Input() speedUnit = SpeedUnit.KM;
   @Input() windSpeed = 0;
   @Input() windDir!: string;
   @Input() humidity = 0;
-  @Input() uv = 0;
+  @Input() uvNumber = 0;
+  uvText = '';
   windDirectionText: any = {
     N: 'Heading from North to South',
     NNE: 'Heading from Northeast to Southwest',
@@ -30,4 +31,21 @@ export class CurrentHighlightsComponent {
     NW: 'Heading from Northwest to Southeast',
     NNW: 'Heading from Northwest to Southeast',
   };
+  ngOnInit(): void {
+    this.setUvText();
+  }
+
+  private setUvText() {
+    if (this.uvNumber < 3) {
+      this.uvText = 'Low. You can safely stay outside';
+    } else if (this.uvNumber < 6) {
+      this.uvText = 'Moderate. Seek shade and use sunscreen';
+    } else if (this.uvNumber < 8) {
+      this.uvText = 'High. Seek shade and use sunscreen';
+    } else if (this.uvNumber < 11) {
+      this.uvText = 'Very High. Avoid being outside if possible';
+    } else if (this.uvNumber >= 11) {
+      this.uvText = 'Extreme. Avoid being outside if possible';
+    }
+  }
 }
